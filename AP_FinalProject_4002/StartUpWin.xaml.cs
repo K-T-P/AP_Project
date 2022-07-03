da9fc2923090db36f7c21d7d69966170c564506e
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2022-07-03 20:19:09 +0430

    Frontend

---
+++ AP_FinalProject_4002/StartUpWin.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AP_FinalProject_4002
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartUpWin : Window
+    {
+        public StartUpWin()
+        {
+            InitializeComponent();
+        }
+
+        private void UserLoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var newWin = new UserLogin_Win();
+            newWin.Show();
+            this.Close();
+        }
+
+        private void UserSignUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AdminLoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AdminLogin_Win newAdminLogin = new AdminLogin_Win();
+            newAdminLogin.Show();
+            this.Close();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ AP_FinalProject_4002/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AP_FinalProject_4002
+{
+    class User: Person
+    {
+        static Dictionary<string, string> users { get; set; } = new Dictionary<string, string>();
+
+        public User(string fname, string lname, string email, string password, int phone)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+            Password = password;
+            PhoneNum = phone;
+            users.Add(Email, Password);
+        }
+
+        long Balance { get; set; } = 0;
+    
+        public static bool SignUp_EmailExistsCheck(string email)
+        {
+            foreach (string key in users.Keys)
+            {
+                if (email.Equals(key))
+                    return false;
+            }
+            return true;
+        }
+        public static bool SignUp_PasswordCheck(string password)
+        {
+            //string pattern = ""
+            //Regex str = new Regex();
+            return false;
+        }
+
+        public static void WalletRecharge()
+        {
+
+        }
+        
+    }
+}
