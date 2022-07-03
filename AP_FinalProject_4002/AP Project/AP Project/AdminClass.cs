b7e2f674e66d9242f63372171b188d2cefc27077
Koroush T.P <michealjabbary@gmail.com>
2022-07-03 23:38:59 +0430

    initial commit

---
+++ AP_FinalProject_4002/AP Project/AP Project/AdminClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace AP_Project
+{
+    class AdminClass
+    {
+        static public ObservableCollection<AdminClass> adminGrp { get; set; }
+        public AdminClass(
+            string f_Name,
+            string l_Name,
+            string email,
+            string password,
+            int p_Number)
+        {
+
+        }
+    }
+}
